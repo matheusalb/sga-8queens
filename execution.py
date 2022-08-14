5d36d7cf9aedec01c048e156177abaae6861705f
Riei-Joaquim <rjmr@cin.ufpe.br>
2022-08-14 18:39:29 -0300

    adding graph for iterations

--- execution.py
+++ execution.py
@@ -2,6 +2,9 @@ from statistics import mean, stdev
 from sga.SGA_8queens import SGA_8queens
 import datetime
 from utils.utils import increment_path
+import matplotlib.pyplot as plt
+import numpy as np
+
 import sys
 
 
@@ -13,7 +16,7 @@ def main():
 
     execution_convergences = 0
     converged_iteraction = []
-    qtd_converged = 0
+    qtd_converged = []
     bests = []
     for i in range(n):
         print('\n----> execution:', i)
@@ -26,7 +29,7 @@ def main():
         if converge:
             execution_convergences += 1
             converged_iteraction.append(final_i)
-            qtd_converged += n_converge
+            qtd_converged.append(n_converge)
 
     generation_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
     path_log = "data/individuals_execution_" + generation_time + ".log"
@@ -41,7 +44,9 @@ def main():
         str(stdev(converged_iteraction)) + '\n')
     individuals.write(
         'Número de indivíduos que convergiram por execução: ' +
-        str(qtd_converged) + '\n')
+        str(qtd_converged) + '\n\t- media = ' +
+        str(mean(qtd_converged)) + '\n\t- std = ' +
+        str(stdev(qtd_converged)) + '\n')
     # individuals.write('Fitness; media = ' + str(mean(converged_iteraction)) + '; std= ' + str(stdev(converged_iteraction))+ '\n')
 
     # for p in population:
@@ -50,6 +55,16 @@ def main():
         individuals.write(str(b.phenotype) + "---> ")
         individuals.write('fitness: ' + str(b.fitness) + "\n")
 
+    #ploat iterations
+    plt.xlabel('Executions')
+    plt.ylabel('Iterations')
+    plt.plot(converged_iteraction,  label="Nº Generations")
+    plt.plot(np.full(len(converged_iteraction),mean(converged_iteraction)), label="Median")
+    plt.legend(loc="upper left")
+
+    generation_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+    path_graph = "data/individuals_execution_" + generation_time + ".png"
+    plt.savefig(path_graph)
 
 if __name__ == '__main__':
     main()
